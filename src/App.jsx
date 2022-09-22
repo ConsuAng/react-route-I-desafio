9d281ea41fd6a33c03b3e1d4608ec6ef48730127
Consue <c.anguitasalinas@gmail.com>
2022-09-22 12:40:11 -0300

    ✨ feat: assembly with Navbar, Footer  and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import NavBar from './components/Navbar'
+import Contact from './views/Contact'
+import Home from './views/Home'
+import NotFound from './views/NotFound'
 
 function App() {
-  
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contacto' element={<Contact />} />
+          <Route path='/*' element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   )
 }
